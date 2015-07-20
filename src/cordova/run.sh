97049d2cb17a079473064ce9d4da6ac97b4890a4
Jürgen Viljaste <j.viljaste@gmail.com>
2015-07-20 16:43:53 +0300

    Update

--- src/cordova/run.sh
+++ src/cordova/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-puppet apply --modulepath=/src/cordova/run/modules /src/cordova/run/run.pp
-
 supervisord -c /etc/supervisor/supervisord.conf
--- src/cordova/run/modules/run/manifests/init.pp
+++
@@ -1,3 +0,0 @@
-class run {
-  include run::cordova
-}
